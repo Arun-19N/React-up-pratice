548e930be21738f40e58303a0d1aeef26c185461
Arun-19N <nuraruran32@gmail.com>
2026-06-19 10:02:01 +0530

    1st

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+     <h1>Arun</h1>
+     <h2>HUHAHUHA 1</h2>
+   </>
+  )
+}
+
+export default App
